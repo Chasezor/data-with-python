a61a85e31a3e29bd454bd24bee174b392835f18f
Chasezor <chase.c.contact@gmail.com>
2021-10-04 16:53:23 -0600

    saving

--- data_presenter.py
+++ data_presenter.py
@@ -1,29 +1,33 @@
+#imports
 import matplotlib.pyplot as plt
 import numpy as np
 from os import close
 
-
+#File
 file1 = open('Cupcakeinvoices.csv')
 
+#Variables
 total =0
 typeArr = []
 amountArr = []
+
+#Logic
 for invoice in file1:
     invoice = invoice.rstrip('\n').split(',')
     print(invoice[2])
     value = float(invoice[3])
     typeArr.append(invoice[2])
-    amountArr.append(value)
     value2= float(invoice[4])
     sum =value * value2   
+    amountArr.append(sum)
     print(sum)
     total += sum
 
-
+#Execution
 print(round(total,2))
 file1.close()
-plt.scatter(amountArr, typeArr)
-plt.bar(typeArr, amountArr)
+#plt.scatter(np.array(typeArr), np.array(amountArr))
+plt.bar(np.array(typeArr), np.array(amountArr))
 plt.show()
 
 
